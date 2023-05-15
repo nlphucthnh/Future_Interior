b4ea659941e1a4e4922d459535021ccc19ef283d
Sinsthnh <100131029+Sinsthnh@users.noreply.github.com>
2023-05-15 09:26:49 +0700

    Start-Project
    Khởi tạo project và thêm project và github

---
+++ ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/AsmFutureInteriorProjectG2Application.java
@@ -0,0 +1,13 @@
+package com.spring.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmFutureInteriorProjectG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmFutureInteriorProjectG2Application.class, args);
+	}
+
+}
---
+++ ASM_Future-Interior_Project-G2/src/test/java/com/spring/main/AsmFutureInteriorProjectG2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmFutureInteriorProjectG2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
