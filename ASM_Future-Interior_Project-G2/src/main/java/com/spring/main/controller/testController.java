c1392a5de60eb8517acbebc7ea97fc24a4f894bb
Thien-CG <lethien3072003@gmail.com>
2023-05-22 22:03:00 +0700

    final cart

--- ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/controller/testController.java
+++ ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/controller/testController.java
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class testController {
-	
-	
-	@RequestMapping(value = "/product/list", method= RequestMethod.GET)
+	@RequestMapping(value = "/home", method= RequestMethod.GET)
 	public String getAboutPage() {
-		return "productlist";
+		return "cart";
 	}
 }
