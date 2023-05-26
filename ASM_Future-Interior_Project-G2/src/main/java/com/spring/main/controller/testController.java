c020839c81a4b2fddad10feae04b86fc180fa35c
Thien-CG <lethien3072003@gmail.com>
2023-05-27 00:09:45 +0700

    Gan xong blog

--- ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/controller/testController.java
+++ ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/controller/testController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class testController {
 	@RequestMapping(value = "/home", method= RequestMethod.GET)
 	public String getAboutPage() {
-		return "cart";
+		return "blog";
 	}
 }
