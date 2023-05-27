eab9619d2d0d94eca8afa5e364bb185a0cc2d74c
Sinsthnh <100131029+Sinsthnh@users.noreply.github.com>
2023-05-27 10:01:25 +0700

    phucThinh - all mangaer page
    + Manage filter
    + Manage account
    + Manage product
    + Manage order
    + Manage  blogs
    + Manage revenue

---
+++ ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/controller/AdminController.java
@@ -0,0 +1,44 @@
+package com.spring.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	
+	
+//	---------------- GET URL MANAGER PAGE ----------------//
+	
+	@GetMapping("/Manager/blog")
+	public String ManageBlogPage() {
+		return "Manager-blog-page";
+	}
+	
+	@GetMapping("/Manager/revenue")
+	public String ManageRevunuePage() {
+		return "Manager-revenue-page";
+	}
+	
+	@GetMapping("/Manager/order")
+	public String ManageOrderPage() {
+		return "Manager-order-page";
+	}
+	
+	@GetMapping("/Manager/product")
+	public String ManageProductPage() {
+		return "Manager-product-page";
+	}
+	
+	
+	@GetMapping("/Manager/filter")
+	public String ManageFilterPage() {
+		return "Manager-filter-page";
+	}
+	
+	@GetMapping("/Manager/account")
+	public String ManageAccountPage() {
+		return "Manager-account-page";
+	}
+	
+	
+}
