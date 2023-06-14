3f26185a7938f386ce806e17f059b91995c2dd73
Thien-CG <lethien3072003@gmail.com>
2023-06-15 02:26:44 +0700

    sap xong quen mk

--- ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/dao/ThongTinTaiKhoanDAO.java
+++ ASM_Future-Interior_Project-G2/src/main/java/com/spring/main/dao/ThongTinTaiKhoanDAO.java
@@ -5,23 +5,31 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.spring.main.entity.TaiKhoan;
 import com.spring.main.entity.ThongTinTaiKhoan;
 
 public interface ThongTinTaiKhoanDAO extends JpaRepository<ThongTinTaiKhoan, Integer> {
 
-    // @Modifying
-    // @Query("UPDATE ThongTinTaiKhoan SET hoTen = :hoTen, gioiTinh = :gioiTinh,
-    // soDienThoai = :soDienThoai WHERE taiKhoanTTTK.tenDangNhap LIKE
-    // :taiKhoanTTTK")
-    // void updateThongTinTaiKhoan(@Param("hoTen") String hoTen, @Param("gioiTinh")
-    // boolean gioiTinh,
-    // @Param("soDienThoai") String soDienThoai, @Param("taiKhoanTTTK") String
-    // taiKhoanTTTK);
+  // @Modifying
+  // @Query("UPDATE ThongTinTaiKhoan SET hoTen = :hoTen, gioiTinh = :gioiTinh,
+  // soDienThoai = :soDienThoai WHERE taiKhoanTTTK.tenDangNhap LIKE
+  // :taiKhoanTTTK")
+  // void updateThongTinTaiKhoan(@Param("hoTen") String hoTen, @Param("gioiTinh")
+  // boolean gioiTinh,
+  // @Param("soDienThoai") String soDienThoai, @Param("taiKhoanTTTK") String
+  // taiKhoanTTTK);
 
   @Modifying
-@Query("UPDATE ThongTinTaiKhoan SET hoTen = :hoTen, gioiTinh = :gioiTinh, soDienThoai = :soDienThoai WHERE taiKhoanTTTK.tenDangNhap LIKE %:taiKhoanTTTK%")
-void updateThongTinTaiKhoan(@Param("hoTen") String hoTen, @Param("gioiTinh") boolean gioiTinh,
-        @Param("soDienThoai") String soDienThoai,
-        @Param("taiKhoanTTTK") String taiKhoanTTTK);
+  @Query("UPDATE ThongTinTaiKhoan SET hoTen = :hoTen, gioiTinh = :gioiTinh, soDienThoai = :soDienThoai WHERE taiKhoanTTTK.tenDangNhap LIKE %:taiKhoanTTTK%")
+  void updateThongTinTaiKhoan(@Param("hoTen") String hoTen, @Param("gioiTinh") boolean gioiTinh,
+      @Param("soDienThoai") String soDienThoai,
+      @Param("taiKhoanTTTK") String taiKhoanTTTK);
+
+  // ThongTinTaiKhoan findBytaiKhoanTTTK(String taiKhoanTTTK);
+
+  @Modifying
+  @Query("UPDATE ThongTinTaiKhoan SET anhDaiDien = :anhDaiDien WHERE taiKhoanTTTK.tenDangNhap LIKE %:taiKhoanTTTK%")
+  void updateAvatar(@Param("anhDaiDien") String anhDaiDien,
+      @Param("taiKhoanTTTK") String taiKhoanTTTK);
 
 }
